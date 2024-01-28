ce48156698391239a97ab3c6605a3c4b67f4a959
jianghui <jianghui@uino.com>
2024-01-28 13:42:09 +0800

    fix: 第一个demo

---
+++ thingjs2.0-demo/src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+setTimeout(async () => {
+  await THING.Utils.login("http://10.100.31.150:8899/processRequest2");
+  setTimeout(() => {
+    const app = new THING.App({
+      url: "/self",
+      onComplete: function (ev: any) {
+        console.log(ev); // 加载完成后回调
+        THING.App.current.levelManager.change(
+          THING.App.current.query(".Campus")[0]
+        );
+      },
+    });
+  }, 1000);
+}, 500);
+</script>
+
+<template>
+  <div id="div3d"></div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ thingjs2.0-demo/src/components/HelloWorld.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>1111111111111111111</h1>
+  <!-- <div id="div3d"></div> -->
+</template>
+<script setup lang="ts"></script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
